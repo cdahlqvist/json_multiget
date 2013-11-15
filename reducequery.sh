83a500b8ecf8579551ecf526841174f37ebad138
Christian Dahlqvist <acd@Christians-MacBook-Air.local>
2013-11-15 18:28:23 +0000

    Corrected reducequery.sh

--- reducequery.sh
+++ reducequery.sh
@@ -1,5 +1,1 @@
-curl -XPOST http://localhost:8098/mapred -H 'Content-Type: application/json' -d '{"inputs":"person","query":[{"map":{"language":"erlang","module":"json_multiget","function":"map_json_multiget"}}]}'
-
 curl -XPOST http://localhost:8098/mapred -H 'Content-Type: application/json' -d '{"inputs":[["notfound","notfound"]],"query":[{"reduce":{"language":"erlang","module":"json_multiget","function":"reduce_json_multiget","arg":"{\"bucket\":\"person\",\"keylist\":[\"bill\",\"sarah\",\"bob\"],\"timeout\":60000,\"concurrency\":5,\"r\":2,\"pr\":0}"}}]}'
-
-
